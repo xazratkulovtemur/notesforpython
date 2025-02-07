03c5cd325f528c8f2085fa862d44f78f418ae970
xazratqulovtemur <khazratkulovtemur@gmail.com>
2025-02-07 17:23:35 +0500

    update

--- lesson-2/string_simple.py
+++ lesson-2/string_simple.py
@@ -2,3 +2,5 @@ word=input("Words is: ")
 last_char=word[-1]
 print(f"Last character is: {word[-1]}", end="\n")   # to find what is last character
 #to be able to write words reversely, we can use for example word[::-1]
+#just test
+print("just test")
